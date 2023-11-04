de01ca3b0c33acd93f4384c7aa98cde57be4c0cb
Goncalo-MMG-Carvalho <gmm.carvalho@campus.fct.unl.pt>
2023-11-04 17:35:25 +0000

    Create the files

---
+++ Sender.py
@@ -0,0 +1,31 @@
+"""
+main()
+ execute actions in the edge conducting to the initial state
+state = INITIAL_STATE
+ while state != FINAL_STATE:
+ match state:
+ case STATE_1:
+ # consider all the edges departing from STATE_1
+ if condition in the first edge considered is true:
+ execute actions described
+ state = state where the edge terminates
+ elif condition in the second link considered is true:
+ execute action specified in the edge
+ …
+ else:
+ case STATE_2:
+ …
+ case STATE_x:
+ …
+ if …:
+ …
+ State = FINAL_STATE
+"""
+
+
+def main():
+    quit(69420)
+
+
+if __name__ == "__main__":
+    main()
