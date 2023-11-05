ae603a34d8e0d0a320838ba88100d14283456f34
Goncalo-MMG-Carvalho <gmm.carvalho@campus.fct.unl.pt>
2023-11-05 20:39:50 +0000

    Funciona, Falta implementar a Window, Falta Remover o hardcode do senderaddr e receiveraddr, Falta fazer/verificar os TODOS, Falta apagar prints desnecessários

--- Receiver.py
+++ Receiver.py
@@ -5,17 +5,32 @@ import sys
 import random
 
 
-def sendDatagram (msg, sock, address):
+STATE_SEND = 1
+STATE_WAIT = 2
+STATE_RECV = 3
+STATE_END = 4
+
+chunkSize = 1024
+dgramSize = chunkSize + sys.getsizeof(int) * 2  # 2 ints for the header
+
+rs = socket(AF_INET, SOCK_DGRAM)
+senderAddr = ()
+
+#TODO: Remove this
+senderAddr = ("127.0.0.1", 10000)
+receiverAddr = ("127.0.0.1", 10001)
+
+def sendDatagram(msg, sock, address):
     # msg is a byte array ready to be sent
     # Generate random number in the range of 1 to 10
     rand = random.randint(1, 10)
 
-    # 20% of loss probability
-    if rand > 2:
+    # 20% of loss probability TODO change this number
+    if rand > 0:
         sock.sendto(msg, address)
 
 
-def waitForReply( uSocket, timeOutInSeconds ):
+def waitForReply(uSocket, timeOutInSeconds):
     rx, tx, er = select.select([uSocket], [], [], timeOutInSeconds)
     # waits for data or timeout
     if not rx:
@@ -26,29 +41,102 @@ def waitForReply( uSocket, timeOutInSeconds ):
 
 def main():
     # Check number of arguments
+    global senderAddr
+
     if len(sys.argv) != 4:
         print("Wrong number of arguments.")
         sys.exit(6969)
 
     port = int(sys.argv[2])  # port number
-    serverAddressPort = (sys.argv[1], port)  # server IP and port
+
     fileName = sys.argv[3]  # name of the file in the receiver
 
     if port < 1024 or port > 65535:
-        print("Incorrect port number.")
+        print("Invalid port number.")
         sys.exit(69420)
 
-    print("Client is running.")
+    # TODO: uncomment this
+    #rs.bind((sys.argv[1], port))
 
-    rs = socket(AF_INET, SOCK_DGRAM)
+    #TODO: Remove this
+    rs.bind(receiverAddr)
+
+    print("Receiver is running.")
 
     # file open
-    file = open("./" + fileName, 'wb')
+    file = open("./receiverFiles/" + fileName, 'wb')
 
     offset = 0
+    writtenPKT = 0
+    confirm = -1
+    state = STATE_WAIT
 
     while True:
-        if waitForReply(rs, 1):
+        if state == STATE_WAIT:
+            if waitForReply(rs, 1):
+                state = STATE_RECV
+
+        elif state == STATE_RECV:
+            print("Receiving packet...")
+            reply, sAddr = rs.recvfrom(dgramSize)
+
+
+            #if senderAddr == ():
+            #    senderAddr = sAddr
+
+            if senderAddr != sAddr:
+                print("Error: Received a packet from a different sender.")
+                continue
+
+            reply = pickle.loads(reply)  # (status, pktNum, data)
+            pktNum = int(reply[1])
+            print("Packet received: ", pktNum, ".")
+
+            if reply[0] != 0:
+                print("Error: Received a non data packet.")
+                continue
+
+            if pktNum == -1:
+                state = STATE_END
+                continue
+
+            if pktNum == writtenPKT:
+                print("Writing packet number ", pktNum, ".")
+                file.write(reply[2])
+                print("Packet written.")
+                offset += len(reply[2])
+                writtenPKT += 1
+                """ 
+                # Verify if there are packets in the buffer that can be written
+                """
+                state = STATE_SEND
+                confirm = writtenPKT
+
+            else:
+                if pktNum < writtenPKT:
+                    # resend ack
+                    state = STATE_SEND
+                    confirm = writtenPKT
+
+                """
+                else:
+                    #add to buffer and wait for the rest of the packets
+                    buffer.append((pktNum, reply[2]))
+                    state = STATE_SEND
+                    confirm = pktNum
+                """
+
+        elif state == STATE_SEND:
+            # send ack
+            ack = pickle.dumps((1, confirm))
+            sendDatagram(ack, rs, senderAddr)
+            state = STATE_WAIT
+
+        elif state == STATE_END: #TODO fix this
+            # send ack
+            ack = pickle.dumps((1, confirm))
+            sendDatagram(ack, rs, senderAddr)
+            print("File transfer complete.")
             break
 
     file.close()
@@ -57,3 +145,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
--- Sender.py
+++ Sender.py
@@ -14,18 +14,24 @@ STATE_RECV = 3
 STATE_TIMEOUT = 4
 STATE_END = 5
 
-size = 1024
+chunkSize = 1024
+dgramSize = chunkSize + sys.getsizeof(int) * 2  # 2 ints for the header
 
 ss = socket(AF_INET, SOCK_DGRAM)
+receiverAddr = ()
+
+#TODO: Remove this
+senderAddr = ("127.0.0.1", 10000)
+receiverAddr = ("127.0.0.1", 10001)
 
 
-def sendDatagram (msg, sock, address):
+def sendDatagram(msg, sock, address):
     # msg is a byte array ready to be sent
     # Generate random number in the range of 1 to 10
     rand = random.randint(1, 10)
 
-    # 20% of loss probability
-    if rand > 2:
+    # 20% of loss probability TODO change this number
+    if rand > 0:
         sock.sendto(msg, address)
 
 
@@ -38,29 +44,75 @@ def waitForReply(uSocket, timeOutInSeconds):
         return True
 
 
-def make_pkt(data):
-    pkt = pickle.dumps((M_DATA, npkt, data))  # 0 indicates that it is a data packet
+def make_pkt(data, pktNum):
+    pkt = pickle.dumps((M_DATA, pktNum, data))  # 0 indicates that it is a data packet
     return pkt
 
 
-def rdt_send(data):
-    pkt = make_pkt(data)
-    sendDatagram(pkt, ss, (receiverIP, receiverPort))
+def rdt_send(data, pktNum):
+    pkt = make_pkt(data, pktNum)
+    sendDatagram(pkt, ss, receiverAddr)
 
 
 def main():
+
+    global receiverAddr
+
+    if len(sys.argv) != 7:
+        print("Wrong number of arguments.")
+        sys.exit(6969)
+
+    senderIP = sys.argv[1]
+    senderPort = int(sys.argv[2])
+
+    #TODO: uncomment this
+    """ 
+    receiverIP = sys.argv[3]
+    receiverPort = int(sys.argv[4])
+    receiverAddr = (receiverIP, receiverPort)
+    """
+
+    fileName = sys.argv[5]
+    windowSize = int(sys.argv[6])
+
+    if senderPort < 1024 or senderPort > 65535:
+        print("Incorrect Sender Port number.")
+        sys.exit(69420)
+
+    """
+    if receiverPort < 1024 or receiverPort > 65535:
+        print("Incorrect Receiver Port number.")
+        sys.exit(69421)
+    """
+
+    if windowSize < 1 or windowSize > 10:
+        print("Invalid window size.")
+        sys.exit(69422)
+
+    print("Sender is running.")
+
+    #TODO: uncomment this
+    #ss.bind((senderIP, senderPort))
+
+    #TODO: Remove this
+    ss.bind(senderAddr)
+
+
+    pktNum = 0
+
     state = STATE_SEND
-    file = open("./" + fileName, 'rb')
+    file = open("./senderFiles/" + fileName, 'rb')
 
     while True:
         if state == STATE_SEND:
-            data = file.read(size)
+            file.seek(pktNum * chunkSize)
+            data = file.read(chunkSize)
 
             if not data:
                 state = STATE_END
-                break
+                continue
 
-            rdt_send(data)
+            rdt_send(data, pktNum)
             state = STATE_WAIT
 
         elif state == STATE_WAIT:
@@ -70,10 +122,10 @@ def main():
                 state = STATE_TIMEOUT
 
         elif state == STATE_RECV:
-            reply, trash = ss.recvfrom(size)
+            reply, trash = ss.recvfrom(dgramSize)
             reply = pickle.loads(reply)
             if int(reply[0]) == M_ACK:  # ack
-                npkt += 1
+                pktNum = int(reply[1])
             else:
                 print("Error: received a data packet instead of an ack.")
             state = STATE_SEND
@@ -81,37 +133,14 @@ def main():
         elif state == STATE_TIMEOUT:
             state = STATE_SEND
 
+        elif state == STATE_END:
+            ss.sendto(pickle.dumps((M_DATA, -1, b'')), receiverAddr)
+            print("File transfer complete.")
+            break
+
+    file.close()
+    ss.close()
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 7:
-        print("Wrong number of arguments.")
-        sys.exit(6969)
-
-    senderIP = sys.argv[1]
-    senderPort = int(sys.argv[2])
-
-    receiverIP = sys.argv[3]
-    receiverPort = int(sys.argv[4])
-
-    fileName = sys.argv[5]
-    window = int(sys.argv[6])
-
-    if senderPort < 1024 or senderPort > 65535:
-        print("Incorrect Sender Port number.")
-        sys.exit(69420)
-
-    if receiverPort < 1024 or receiverPort > 65535:
-        print("Incorrect Receiver Port number.")
-        sys.exit(69421)
-
-    if window < 1 or window > 10:
-        print("Incorrect window size.")
-        sys.exit(69422)
-
-    print("Sender is running.")
-
-    ss.bind((senderIP, senderPort))
-
-    npkt = 0
-
     main()
